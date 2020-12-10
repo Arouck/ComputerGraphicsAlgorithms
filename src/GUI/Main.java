d47298bd5846009c686cece701eabd1b773523ac
Arouck <ppvitorr@gmail.com>
2020-12-10 19:39:01 -0300

    Remove unused stuff from Main.java file

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,24 +8,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public Stage primaryStage;
-
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-       /* */
 
         stage.setTitle("Computer Graphics Algorithms");
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
-
-        /*Pane drawPane = (Pane) scene.lookup("#drawPane");
-
-        drawPane.getChildren().add(path);*/
-
-        primaryStage = stage;
 
         stage.show();
     }
